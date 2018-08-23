e32adba01463eeb38d4f4542f802acbcce5812e3
outsiders-arena <remote1992@ail.com>
2018-08-23 01:16:57 -0500

    refining matchmaking

--- WebContent/resources/js/battle.js
+++ WebContent/resources/js/battle.js
@@ -1,20 +1,29 @@
 var ws = null;
 
 function findBattle() {
-	var id = $("#arenaIdInput").val().toString();
-	if (id.length > 0) {
-		connectByArenaId(id);
+	var playerId = $("#playerIdMatchMakingInput").val().toString() || "";
+	if (playerId.length > 0){
+		connectByPlayerId(playerId);
 	} else {
 		connect();
 	}
 }
 
-function connectByPlayerId() {
+function connectByPlayerId(id) {
 	
+	$.ajax({method: "GET", async: "FALSE", url: "http://66.242.90.163:8171/api/player/arena/" + id}).done(function(result) {
+		console.log(result);
+		var arenaId = result;
+		ws = new WebSocket('ws://66.242.90.163:8171/arena/' + arenaId);
+
+		console.log("Connected to Friend!");
+		console.log(ws);
+		handleMessage();
+		setTimeout(sendMatchMakingMessage, 5000);
+	});
 }
 
 function connectByArenaId(id) {
-	var playerId = Math.random().toString().substring(2, 10);
 	ws = new WebSocket('ws://66.242.90.163:8171/arena/' + id);
 	$("#arenaId").append(id);
 	console.log("Connected to Friend!");
@@ -63,14 +72,13 @@ function handleMessage() {
 }
 
 function sendConnectRequest() {
-	var disp = $("#displayNameInput").val().toString();
-	var pid = $("#playerIdInput").val();
-	var aurl = $("#avatarUrlInput").val().toString();
+	var disp = $("#displayNameInput").val().toString() || "NPC";
+	var aurl = $("#avatarUrlInput").val().toString() || "defaultImageUrl";
 	var arid = $("#arenaId").text();
 	var aid = arid.substring(arid.length - 7, arid.length);
+	console.log(aid);
 	var req = {
 			"displayName": disp,
-			"playerId": pid,
 			"avatarUrl": aurl,
 			"currentArena": aid
 	};
@@ -80,6 +88,7 @@ function sendConnectRequest() {
 }
 
 function afterLogin(result) {
+	$("#playerId").append(result.id);
 	console.log(result);
 }
 
@@ -113,15 +122,16 @@ function handleTurnEnd(msg) {
 
 function sendMatchMakingMessage() {
 	var chars = $("#chars").val()
+	var playerId = $("#playerId").text().substring($("#playerId").text().length - 8, $("#playerId").text().length);
+	console.log(playerId);
 	console.log(chars);
 	var msg = JSON.stringify({
 		type: "MATCH_MAKING",
 		char1: chars[0],
 		char2: chars[1],
 		char3: chars[2],
-		playerId: $("#playerIdInput").val().toString(),
-		opponentId: $("#playerIdMatchMakingInput").val().toString(),
-		arenaId: $("#arenaIdInput").val().toString()
+		playerId: playerId,
+		opponentId: $("#playerIdMatchMakingInput").val().toString()
 	});
 	console.log(msg);
 	ws.send(msg);
@@ -159,7 +169,7 @@ $(document).ready(function(){
     	disconnect();
     }
     
-	var rand = Math.random().toString().substring(2, 9);
+	var rand = Math.random().toString().substring(3, 9);
 	$("#arenaId").append(rand);
     
     $("select option").click(function() {
